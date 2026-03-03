f6fb29ac91b997621d1b6868b42ce0d19d378bce
r00nbun <r00nbun@gmail.com>
2026-03-03 18:44:47 +0300

    Finished

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,113 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Text } from 'src/ui/text';
+import { Separator } from 'src/ui/separator';
+
+import { useRef, useState, FormEvent } from 'react';
+
+import {
+	OptionType,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (value: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: ArticleParamsFormProps) => {
+	const asideRef = useRef<HTMLElement | null>(null);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [formState, setFormState] = useState<ArticleStateType>(articleState);
+
+	const toggleArrow = () => {
+		setIsMenuOpen((prev) => !prev);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		setArticleState(defaultArticleState);
+	};
+
+	const handleApply = (e: FormEvent) => {
+		e.preventDefault();
+		setArticleState(formState);
+	};
+
+	const handleChange = (key: keyof ArticleStateType, value: OptionType) => {
+		setFormState((prev) => ({
+			...prev,
+			[key]: value,
+		}));
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isMenuOpen} onClick={toggleArrow} />
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isMenuOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleApply}
+					onReset={handleReset}>
+					<Text size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(op) => handleChange('fontFamilyOption', op)}
+					/>
+
+					<RadioGroup
+						title='Размер шрифта'
+						name='fontSize'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(op) => handleChange('fontSizeOption', op)}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(op) => handleChange('fontColor', op)}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(op) => handleChange('backgroundColor', op)}
+					/>
+
+					<Select
+						title='Ширина контейнера'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(op) => handleChange('contentWidth', op)}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
